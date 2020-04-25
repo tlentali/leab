6a80556849be20874b061220baf004abfdd6729e
tlentali <thomas.lentali@gmail.com>
2020-04-25 17:31:40 +0200

    data sample pathlib usage

--- leab/leDataset/__init__.py
+++ leab/leDataset/__init__.py
@@ -1,5 +1,5 @@
 from .sample_le_success import SampleLeSuccess
-from .sample_le_mean import SampleLeMean
+from .sample_le_average import SampleLeAverage
 
 __all__ = [
     'SampleLeSuccess',
--- leab/leDataset/sample_le_average.py
+++ leab/leDataset/sample_le_average.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pathlib import Path
 
 
 class SampleLeAverage:
@@ -6,9 +7,14 @@ class SampleLeAverage:
     Sample data to run with leAverage, from E. Miller default example in app.
     """
     def __init__(self):
+
+        root = Path(__file__).parent.parent.parent
+        file_1 = root / 'data' / 'evan_miller_ttest_default_1.csv'
+        file_2 = root / 'data' / 'evan_miller_ttest_default_2.csv'
+
         self.A = pd.read_csv(
-            "../../data/evan_miller_ttest_default_1.csv", names=["values"]
+            file_1, names=["values"]
         )
         self.B = pd.read_csv(
-            "../../data/evan_miller_ttest_default_2.csv", names=["values"]
+            file_2, names=["values"]
         )
