9f3d50b2d2b08c0c3b61da1a907c7e7bc16b57ed
tlentali <thomas.lentali@gmail.com>
2020-05-02 18:11:15 +0200

    return verdict leSuccess instead print + unittest

---
+++ tests/test_leSuccess.py
@@ -0,0 +1,24 @@
+from leab import after
+from leab import leDataset
+
+
+def test_get_confidence_interval():
+    data = leDataset.SampleLeSuccess()
+    ab_test = after.leSuccess(data.A, 
+                              data.B, 
+                              confidence_level=0.95)
+    assert ab_test.sample_A.confidence_interval == [8.526343659939133, 22.13718821096384]
+
+def test_get_p_value():
+    data = leDataset.SampleLeSuccess()
+    ab_test = after.leSuccess(data.A, 
+                              data.B, 
+                              confidence_level=0.95)
+    assert ab_test.p_value == 0.25870176105718934
+
+def test_get_verdict():
+    data = leDataset.SampleLeSuccess()
+    ab_test = after.leSuccess(data.A, 
+                              data.B, 
+                              confidence_level=0.95)
+    assert ab_test.get_verdict() == "No significant difference"
